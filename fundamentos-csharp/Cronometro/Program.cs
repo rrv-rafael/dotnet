547c0cceba61a1a1d19f9360e313339b8f61a482
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-05-15 20:43:28 -0300

    Cronômetro usando console application

---
+++ fundamentos-csharp/Cronometro/Program.cs
@@ -0,0 +1,66 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S - Segundo => 10s = 10 segundos\nM - Minuto => 1m = 1 minuto\n0 - Sair");
+
+    Console.WriteLine("Quanto tempo deseja contar?");
+    var tempoInformado = Console.ReadLine()?.ToLower();
+    var tipo = tempoInformado != null && tempoInformado.Equals("0") ? "0" : tempoInformado?.Substring(tempoInformado.Length - 1, 1);
+    var tempo = tempoInformado?[..^1] != null && tipo != "0" ? int.Parse(tempoInformado[..^1]) : 0;
+
+    switch (tipo)
+    {
+        case "s":
+            PreStart(tempo);
+            break;
+        case "m":
+            PreStart(ConverterMinutoParaSegundo(tempo));
+            break;
+        case "0":
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Tipo inválido.");
+            break;
+    }
+}
+
+static int ConverterMinutoParaSegundo(int minuto)
+{
+    return minuto * 60;
+}
+
+static void PreStart(int tempo)
+{
+    Console.Clear();
+    Console.WriteLine("Preparar...");
+    Thread.Sleep(1000);
+
+    Console.WriteLine("Apontar...");
+    Thread.Sleep(1000);
+
+    Console.WriteLine("Fogo...");
+    Thread.Sleep(2500);
+
+    Start(tempo);
+}
+
+static void Start(int tempo)
+{
+    int tempoAtual = 0;
+
+    while (tempoAtual < tempo)
+    {
+        Console.Clear();
+        tempoAtual++;
+        Console.WriteLine(tempoAtual);
+        Thread.Sleep(1000);
+    }
+
+    Console.Clear();
+    Console.WriteLine("Cronomêtro finalizado!");
+    Thread.Sleep(2500);
+    Menu();
+}
--- fundamentos-csharp/Cronometro/obj/Debug/net8.0/Cronometro.AssemblyInfo.cs
+++ fundamentos-csharp/Cronometro/obj/Debug/net8.0/Cronometro.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Cronometro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4fdaeb4c77643be77e210b22e678fa2e63e26f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
+[assembly: System.Reflection.AssemblyProductAttribute("Cronometro")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Cronometro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
