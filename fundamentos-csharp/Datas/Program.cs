27382aafab6467096d15b8fc8dfd6dea62d2d3f0
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-06-23 15:46:27 -0300

    Adicionando valores.

--- fundamentos-csharp/Datas/Program.cs
+++ fundamentos-csharp/Datas/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Clear();
 
-//var data = new DateTime();
 var data = DateTime.Now;
-
 Console.WriteLine(data);
+Console.WriteLine(data.AddDays(12));
+Console.WriteLine(data.AddMonths(1));
+Console.WriteLine(data.AddYears(1));
