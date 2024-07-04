4dbe9cc5cf5e561c62ad81e985d9096586a6464d
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-07-03 21:20:20 -0300

    Math.

--- fundamentos-csharp/Moedas/Program.cs
+++ fundamentos-csharp/Moedas/Program.cs
@@ -3,4 +3,7 @@
 Console.Clear();
 
 decimal valor = 10536.25m;
-Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+Console.WriteLine(Math.Round(valor));
+Console.WriteLine(Math.Ceiling(valor));
+Console.WriteLine(Math.Floor(valor));
+// Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
