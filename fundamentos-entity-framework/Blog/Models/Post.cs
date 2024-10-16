2c3ce6ef79cb12813542d3c5258a70649634f3cc
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-10-16 20:05:51 -0300

    Criação dos modelos.

---
+++ fundamentos-entity-framework/Blog/Models/Post.cs
@@ -0,0 +1,15 @@
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
