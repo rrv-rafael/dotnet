8b49bd3a6a7786036ce4c7a79c98a8a6538f2fd2
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-10-17 20:26:31 -0300

    Implementação do UPDATE.

--- fundamentos-entity-framework/Blog/Program.cs
+++ fundamentos-entity-framework/Blog/Program.cs
@@ -9,8 +9,11 @@ class Program
     {
         using var context = new BlogDataContext();
 
-        var tag = new Tag { Name = "ASP .NET", Slug = "aspnet" };
-        context.Tags.Add(tag);
+        var tag = context.Tags.FirstOrDefault(x => x.Id == 1) ?? throw new InvalidOperationException("Tag not found.");
+        tag.Name = ".NET";
+        tag.Slug = "dotnet";
+
+        context.Update(tag);
         context.SaveChanges();
     }
 }
--- fundamentos-entity-framework/Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ fundamentos-entity-framework/Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d7f9011f1e873dcd45921a10cf55c4d4d868bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dff67fce50c999d1854493d4163359fc0c60220")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
