a47041888bea6ed03dc6224f67fc258d5f88c990
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-10-22 19:35:11 -0300

    DataAnnotations, usando Table, Key e Identity.

--- fundamentos-entity-framework/Blog/Models/Category.cs
+++ fundamentos-entity-framework/Blog/Models/Category.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
--- fundamentos-entity-framework/Blog/Program.cs
+++ fundamentos-entity-framework/Blog/Program.cs
@@ -9,9 +9,5 @@ class Program
     static void Main(string[] args)
     {
         using var context = new BlogDataContext();
-
-        var tag = context.Tags.AsNoTracking().FirstOrDefault(x => x.Id == 1) ?? throw new InvalidOperationException("Tag is not found");
-
-        Console.WriteLine(tag.Name);
     }
 }
