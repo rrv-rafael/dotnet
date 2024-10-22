59097a2aaba13771c55c47d27fd677bf15a4cc84
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-10-22 19:46:43 -0300

    DataAnnotations. Navigation Properties.

--- fundamentos-entity-framework/Blog/Models/Post.cs
+++ fundamentos-entity-framework/Blog/Models/Post.cs
@@ -1,15 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("Post")]
     public class Post
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public int AuthorId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Summary { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        public required Category Category { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+        public required User Author { get; set; }
     }
 }
--- fundamentos-entity-framework/Blog/Models/User.cs
+++ fundamentos-entity-framework/Blog/Models/User.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
