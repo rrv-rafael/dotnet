ae9c2afb79702b269ad6cdd346e939860cd36476
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-10-24 20:04:44 -0300

    O commit anterior foi sobre chave primária e identity, este é de mapeamento de propriedades.

--- fundamentos-entity-framework/FluentBlog/Data/Mappings/CategoryMap.cs
+++ fundamentos-entity-framework/FluentBlog/Data/Mappings/CategoryMap.cs
@@ -11,6 +11,8 @@ namespace FluentBlog.Data.Mappings
             builder.ToTable("Category");
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasColumnName("Name").HasColumnType("NVARCHAR").HasMaxLength(80);
+            builder.Property(x => x.Slug).IsRequired().HasColumnName("Slug").HasColumnType("VARCHAR").HasMaxLength(80);
         }
     }
 }
