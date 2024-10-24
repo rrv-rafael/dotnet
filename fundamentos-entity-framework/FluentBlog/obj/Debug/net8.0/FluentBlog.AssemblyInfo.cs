50ec3631f1e586e78d58c52fddcf435c3dd285c9
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-10-24 19:55:45 -0300

    Criação do CategoryMap.

--- fundamentos-entity-framework/FluentBlog/obj/Debug/net8.0/FluentBlog.AssemblyInfo.cs
+++ fundamentos-entity-framework/FluentBlog/obj/Debug/net8.0/FluentBlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FluentBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474d9a4b1c3b78314006b4cb6ecdde661ce7090c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc0cd3bde9bd0b43cd4a3fbeda751b9039458eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FluentBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FluentBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
