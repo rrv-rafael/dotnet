a9b684613877d8afc93cff7e1e62814b1b2d53fc
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-11-05 19:59:35 -0300

    Utilizando os mapeamentos criados.

--- fundamentos-entity-framework/FluentBlog/Data/FluentBlogDataContext.cs
+++ fundamentos-entity-framework/FluentBlog/Data/FluentBlogDataContext.cs
@@ -1,13 +1,25 @@
+using FluentBlog.Data.Mappings;
+using FluentBlog.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FluentBlog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=FluentBlog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
             optionsBuilder.LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
         }
     }
 }
--- fundamentos-entity-framework/FluentBlog/Data/Mappings/UserMap.cs
+++ fundamentos-entity-framework/FluentBlog/Data/Mappings/UserMap.cs
@@ -22,6 +22,11 @@ namespace FluentBlog.Data.Mappings
             builder.Property(x => x.Bio).IsRequired().HasColumnName("Bio").HasColumnType("TEXT");
 
             builder.HasIndex(x => x.Slug, "IX_User_Slug").IsUnique();
+
+            builder.HasMany(x => x.Roles).WithMany(x => x.Users).UsingEntity<Dictionary<string, object>>(
+                "UserRole",
+                role => role.HasOne<Role>().WithMany().HasForeignKey("RoleId").HasConstraintName("FK_UserRole_RoleId").OnDelete(DeleteBehavior.Cascade),
+                user => user.HasOne<User>().WithMany().HasForeignKey("UserId").HasConstraintName("FK_UserRole_UserId").OnDelete(DeleteBehavior.Cascade));
         }
     }
 }
