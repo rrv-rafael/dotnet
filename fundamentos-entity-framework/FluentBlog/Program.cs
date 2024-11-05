8d62496336d7098d186741c86ff8817a67eaeb1c
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2024-11-05 20:25:53 -0300

    Limpando a Main e instalando o dotnet-ef (comando: 'dotnet tool install --global dotnet-ef')

--- fundamentos-entity-framework/FluentBlog/Program.cs
+++ fundamentos-entity-framework/FluentBlog/Program.cs
@@ -7,26 +7,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        using var context = new FluentBlogDataContext();
-
-        var user = context.Users.FirstOrDefault();
         
-        var post = new Post
-        {
-            Author = user,
-            Body = "Meu artigo",
-            Category = new Category
-            {
-                Name = "Backend",
-                Slug = "backend"
-            },
-            CreateDate = DateTime.Now,
-            Slug = "meu-artigo",
-            Summary = "Neste artigo vamos conferir...",
-            Title = "Meu artigo",
-        };
-        
-        context.Posts.Add(post);
-        context.SaveChanges();
     }
 }
